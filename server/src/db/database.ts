193346c41cae5540d4ca368e9cf093c56ab46a88
Lucas <lucas@insidedb.com.br>
2024-10-02 17:17:24 -0400

    feat(db): add method to search unique record by field

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -100,6 +100,49 @@ class Database {
     return result.rows as T[];
   }
 
+  public static async searchUniqueByField<T>(
+    table: string,
+    field: string,
+    value: any,
+    fields: string[] | null = null,
+  ): Promise<T | null> {
+    let query: string;
+
+    if (fields && fields.length > 0) {
+      if (fields.length === 1) {
+        query = `
+          SELECT ${fields[0]}
+          FROM ${table}
+          WHERE ${field} = $1
+          LIMIT 1;
+        `;
+      } else {
+        const selectedFields = fields.join(', ');
+        query = `
+          SELECT ${selectedFields}
+          FROM ${table}
+          WHERE ${field} = $1
+          LIMIT 1;
+        `;
+      }
+    } else {
+      query = `
+        SELECT *
+        FROM ${table}
+        WHERE ${field} = $1
+        LIMIT 1;
+      `;
+    }
+
+    const result = await this.pool.query(query, [value]);
+
+    if (result.rows.length > 0) {
+      return result.rows[0] as T;
+    } else {
+      return null;
+    }
+  }
+
   public static async deleteFromTable(
     tableName: string,
     id: number | string,
