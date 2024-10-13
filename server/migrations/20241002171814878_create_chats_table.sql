0b3d20b2baf3edc1be7e8512f21cf6b0b82e467c
Lucas Soares <lucassoares2007@hotmail.com>
2024-10-13 08:35:35 -0300

    feat(migrations): add profile_pic_url column to chats table

--- server/migrations/20241002171814878_create_chats_table.sql
+++ server/migrations/20241002171814878_create_chats_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE "chats" (
   "id" TEXT NOT NULL,
   "name" VARCHAR(255) NOT NULL,
   "is_group" BOOLEAN NOT NULL DEFAULT false,
+  "profile_pic_url" TEXT,
   CONSTRAINT "chats_pkey" PRIMARY KEY ("id")
 );
 
