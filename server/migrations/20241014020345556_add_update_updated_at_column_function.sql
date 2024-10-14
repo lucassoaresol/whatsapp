1bc81ce1eafdb164fac6838b7c407bab7f68a406
Lucas Soares <lucassoares2007@hotmail.com>
2024-10-13 23:13:11 -0300

    feat(migrations): add function for updating updated_at and alter chats table with timestamps

---
+++ server/migrations/20241014020345556_add_update_updated_at_column_function.sql
@@ -0,0 +1,11 @@
+-- up
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- down
+DROP FUNCTION IF EXISTS update_updated_at_column();
