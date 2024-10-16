2a54ba632e1bd2aa2fbb1e5c135417d44ce2188f
Lucas <lucas@insidedb.com.br>
2024-10-16 13:44:46 -0300

    refactor(jobs): improve logging and timing control in chat and message jobs

--- server/src/job/chatTransferJob.ts
+++ server/src/job/chatTransferJob.ts
@@ -1,25 +1,49 @@
 import { CronJob } from 'cron';
+import { Dayjs } from 'dayjs';
 
+import dayLib from '../libs/dayjs';
 import { getChatManager } from '../models/chatManager';
 
 let isRunning = false;
+let lastLogTime: Dayjs | null = null;
+const jobName = 'CarregarDadosChatManager';
 
 CronJob.from({
   cronTime: '*/3 * * * * *',
   onTick: async () => {
+    const now = dayLib();
+
     if (isRunning) {
-      console.log('Job já está em execução, ignorando a nova execução.');
+      if (!lastLogTime || now.diff(lastLogTime, 'seconds') >= 60) {
+        console.info(
+          `[INFO - ${now.format('YYYY-MM-DD HH:mm:ss')}] [${jobName}] Job já está em execução, ignorando nova execução.`,
+        );
+        lastLogTime = now;
+      }
       return;
     }
 
     isRunning = true;
+    console.info(
+      `[INFO - ${now.format('YYYY-MM-DD HH:mm:ss')}] [${jobName}] Job iniciado.`,
+    );
+
     try {
       const chatManager = getChatManager();
       await chatManager.loadDataFromDatabase();
+      console.info(
+        `[INFO - ${now.format('YYYY-MM-DD HH:mm:ss')}] [${jobName}] Dados carregados com sucesso.`,
+      );
     } catch (error) {
-      console.error('Erro durante a execução do job:', error);
+      console.error(
+        `[ERROR - ${now.format('YYYY-MM-DD HH:mm:ss')}] [${jobName}] Erro durante a execução do job:`,
+        error,
+      );
     } finally {
       isRunning = false;
+      console.info(
+        `[INFO - ${now.format('YYYY-MM-DD HH:mm:ss')}] [${jobName}] Job finalizado.`,
+      );
     }
   },
   start: true,
