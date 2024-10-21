193f62265900ffa0ff91723a3e47247f88105faa
Lucas Soares <lucassoares2007@hotmail.com>
2024-10-21 03:35:15 -0300

    refactor(migrations): remove created_at, updated_at and trigger from medias migration

--- server/migrations/20241002171815051_create_medias_table.sql
+++ server/migrations/20241002171815051_create_medias_table.sql
@@ -4,17 +4,8 @@ CREATE TABLE "medias" (
   "mime_type" TEXT,
   "data" TEXT,
   "path" TEXT NOT NULL,
-  "is_down" BOOLEAN NOT NULL DEFAULT false,
-  "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+  "is_down" BOOLEAN NOT NULL DEFAULT false
 );
 
-CREATE TRIGGER update_medias_updated_at
-BEFORE UPDATE ON "medias"
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
 -- down
-DROP TRIGGER IF EXISTS update_medias_updated_at ON "medias";
-
 DROP TABLE "medias";
