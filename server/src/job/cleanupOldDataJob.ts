c9364058390185dd8c13fd3b8d10b043a32c0a77
Lucas Soares <lucassoares2007@hotmail.com>
2024-10-21 03:46:52 -0300

    refactor(job): remove deleteOldMedias from cleanupOldDataJob

--- server/src/job/cleanupOldDataJob.ts
+++ server/src/job/cleanupOldDataJob.ts
@@ -1,13 +1,12 @@
 import { CronJob } from 'cron';
 
-import { deleteOldMedias } from '../utils/deleteOldMedias';
 import { deleteOldMessages } from '../utils/deleteOldMessages';
 import { deleteOldVotes } from '../utils/deleteOldVotes';
 
 CronJob.from({
   cronTime: '0 0 * * *',
   onTick: async () => {
-    await Promise.all([deleteOldMessages(), deleteOldVotes(), deleteOldMedias()]);
+    await Promise.all([deleteOldMessages(), deleteOldVotes()]);
   },
   start: true,
 });
