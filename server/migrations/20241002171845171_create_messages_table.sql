ac9e6827789bad191a53b6963b7448ef66a3efa6
Lucas Soares <lucassoares2007@hotmail.com>
2024-10-21 03:36:47 -0300

    feat(migrations): add status_id, updated_at column and trigger to messages migration

--- server/migrations/20241002171845171_create_messages_table.sql
+++ server/migrations/20241002171845171_create_messages_table.sql
@@ -3,15 +3,24 @@ CREATE TABLE "messages" (
   "id" TEXT NOT NULL PRIMARY KEY,
   "body" TEXT NOT NULL,
   "from_me" BOOLEAN NOT NULL DEFAULT false,
-  "is_new" BOOLEAN NOT NULL DEFAULT true,
-  "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "status_id" INT NOT NULL,
   "chat_id" INTEGER NOT NULL,
   "from_id" TEXT,
   "media_id" INTEGER,
+  "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT "messages_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "status_types" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT "messages_chat_id_fkey" FOREIGN KEY ("chat_id") REFERENCES "clients_chats" ("key") ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT "messages_from_id_fkey" FOREIGN KEY ("from_id") REFERENCES "chats" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT "messages_media_id_fkey" FOREIGN KEY ("media_id") REFERENCES "medias" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE TRIGGER update_messages_updated_at
+BEFORE UPDATE ON "messages"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
 -- down
+DROP TRIGGER IF EXISTS update_messages_updated_at ON "messages";
+
 DROP TABLE "messages";
