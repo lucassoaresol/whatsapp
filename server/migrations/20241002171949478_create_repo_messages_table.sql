766cdf23dc2405159d46f93cbc431978e3e41909
Lucas Soares <lucassoares2007@hotmail.com>
2024-10-21 03:37:07 -0300

    feat(migrations): add status_id foreign key to repo_messages migration

--- server/migrations/20241002171949478_create_repo_messages_table.sql
+++ server/migrations/20241002171949478_create_repo_messages_table.sql
@@ -1,11 +1,12 @@
 -- up
 CREATE TABLE "repo_messages" (
   "id" SERIAL NOT NULL PRIMARY KEY,
-  "is_new" BOOLEAN NOT NULL DEFAULT false,
+  "status_id" INT NOT NULL,
   "msg_id" TEXT NOT NULL,
   "chat_id" TEXT NOT NULL,
   "client_id" TEXT NOT NULL,
-  "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+  "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT "repo_messages_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "status_types" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- down
