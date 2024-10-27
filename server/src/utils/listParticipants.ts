7721f9c15df3c54be6e269f9b77033731dd0f045
Lucas Soares <lucassoares2007@hotmail.com>
2024-10-27 03:52:09 -0300

    chore(listParticipants): add ordering by name in participants query

--- server/src/utils/listParticipants.ts
+++ server/src/utils/listParticipants.ts
@@ -4,10 +4,10 @@ export async function listParticipants(groupId: string) {
   const database = await databasePromise;
 
   const participants = await database.query(
-    `
-  SELECT c.id, c."name", c.profile_pic_url FROM groups_chats gc
+    `SELECT c.id, c."name", c.profile_pic_url FROM groups_chats gc
   JOIN chats c on c.id = gc.chat_id
-  WHERE gc.group_id = $1`,
+  WHERE gc.group_id = $1
+  ORDER BY c."name";`,
     [groupId],
   );
 
