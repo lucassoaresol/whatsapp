26ebd1938842df408df62b4c93c63b77f7ff5c54
Lucas Soares <lucassoares2007@hotmail.com>
2024-10-28 10:40:17 -0300

    feat(migrations): add trigger for notifications on repo_messages table

--- server/migrations/20241002171949478_create_repo_messages_table.sql
+++ server/migrations/20241002171949478_create_repo_messages_table.sql
@@ -9,5 +9,12 @@ CREATE TABLE "repo_messages" (
   CONSTRAINT "repo_messages_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "status_types" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE TRIGGER notify_repo_messages_new_record
+AFTER INSERT ON "repo_messages"
+FOR EACH ROW
+EXECUTE FUNCTION notify_new_record();
+
 -- down
+DROP TRIGGER IF EXISTS notify_repo_messages_new_record ON "repo_messages";
+
 DROP TABLE "repo_messages";
