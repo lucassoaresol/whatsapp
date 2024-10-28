fd62a40021f1a2f0bb9f73aa08f240765b317ebc
Lucas <lucas@insidedb.com.br>
2024-10-28 18:09:57 -0300

    refactor(models): streamline RepoVote to use Vote model for saving

--- server/src/models/repoVote.ts
+++ server/src/models/repoVote.ts
@@ -1,43 +1,28 @@
-import databasePromise from '../libs/database';
-
-import { getClientManager } from './clientManager';
+import Vote from './vote';
 
 class RepoVote {
+  private isSaved = false;
+
   constructor(
     private selectedName: string,
     private chatId: string,
     private clientId: string,
-    private id?: number,
   ) {}
 
   public async save() {
-    const database = await databasePromise;
-    const repoVoteDTO = await database.insertIntoTable({
-      table: 'repo_votes',
-      dataDict: {
-        selected_name: this.selectedName,
-        chat_id: this.chatId,
-        client_id: this.clientId,
-      },
-      select: { id: true },
-    });
-    const repoVote = repoVoteDTO as { id: number };
-    this.id = repoVote.id;
+    const vote = new Vote(this);
+
+    this.isSaved = await vote.process();
+
+    return this.isSaved;
   }
 
-  public async destroy() {
-    const database = await databasePromise;
-
-    await database.deleteFromTable({ table: 'repo_votes', where: { id: this.id! } });
-  }
-
-  public async getClientWPP() {
-    const clientManager = await getClientManager();
-    const client = clientManager.getClient(this.clientId);
-    if (client) {
-      const clientWPP = client.getWpp();
-      if (clientWPP.info) return clientWPP;
-    }
+  public getData() {
+    return {
+      selectedName: this.selectedName,
+      chatId: this.chatId,
+      clientId: this.clientId,
+    };
   }
 }
 
