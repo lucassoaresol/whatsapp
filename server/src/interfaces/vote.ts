9e57db318f28ced51554bf6ae5bb1153f70c58c9
Lucas <lucas@insidedb.com.br>
2024-10-28 18:08:19 -0300

    feat(interfaces): add Vote interface

---
+++ server/src/interfaces/vote.ts
@@ -0,0 +1,13 @@
+export interface IVote {
+  id: number;
+  selected_name: number;
+  is_new: true;
+  chat_id: number;
+  created_at: Date;
+}
+
+export interface IRepoVote {
+  selected_name: string;
+  chat_id: string;
+  client_id: string;
+}
