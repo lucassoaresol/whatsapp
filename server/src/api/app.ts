b7f9aa1153a153f0cc817aefd81261f0764e7692
Lucas <lucas@insidedb.com.br>
2025-04-14 13:08:11 -0300

    feat(monitoring): add Bull Board for monitoring queues

--- server/src/api/app.ts
+++ server/src/api/app.ts
@@ -1,17 +1,36 @@
+import { createBullBoard } from '@bull-board/api';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js';
+import { ExpressAdapter } from '@bull-board/express';
 import cors from 'cors';
 import express from 'express';
 
 import { ClientManagerPromise } from '../models/clientManager.js';
+import { chatQueue } from '../worker/services/chat.js';
+import { messageQueue } from '../worker/services/message.js';
+import { voteQueue } from '../worker/services/vote.js';
 
 import verifyClient from './middlewares/verifyClient.js';
 import verifyClientConnect from './middlewares/verifyClientConnect.js';
 import clientRouter from './routes/client.js';
 import clientManagerRouter from './routes/clientManager.js';
 
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
+
+createBullBoard({
+  queues: [
+    new BullMQAdapter(chatQueue),
+    new BullMQAdapter(messageQueue),
+    new BullMQAdapter(voteQueue),
+  ],
+  serverAdapter: serverAdapter,
+});
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/admin/queues', serverAdapter.getRouter());
 app.use('/static', express.static('public'));
 
 app.use(async (req, res, next) => {
