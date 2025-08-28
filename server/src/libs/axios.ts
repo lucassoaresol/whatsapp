1cbd4bae8ceec4bff7c240aed24a7105503d052c
Lucas <lucas@insidedb.com.br>
2025-08-28 18:37:50 -0300

    feat(api): add sendMessage util in libs/axios

--- server/src/libs/axios.ts
+++ server/src/libs/axios.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { env } from '../config/env.js';
-import { IChatWpp } from '../interfaces/chat.js';
-import { IMediaWpp } from '../interfaces/media.js';
-import { IMessageWpp } from '../interfaces/message.js';
+import { env } from "../config/env.js";
+import { IChatWpp } from "../interfaces/chat.js";
+import { IMediaWpp } from "../interfaces/media.js";
+import { IMessageWpp } from "../interfaces/message.js";
 
 const apiUsingNow = axios.create({
   baseURL: `http://localhost:${env.port}`,
@@ -11,18 +11,25 @@ const apiUsingNow = axios.create({
 });
 
 export const retriveChatWpp = async (clientId: string, chatId: string) => {
-  const { data } = await apiUsingNow.get<IChatWpp>(`${clientId}/chats/${chatId}/wpp`);
+  const { data } = await apiUsingNow.get<IChatWpp>(
+    `${clientId}/chats/${chatId}/wpp`,
+  );
 
   return data;
 };
 
 export const retriveMessageWpp = async (clientId: string, msgId: string) => {
-  const { data } = await apiUsingNow.get<IMessageWpp>(`${clientId}/messages/${msgId}`);
+  const { data } = await apiUsingNow.get<IMessageWpp>(
+    `${clientId}/messages/${msgId}`,
+  );
 
   return data;
 };
 
-export const retriveMessageContactWpp = async (clientId: string, msgId: string) => {
+export const retriveMessageContactWpp = async (
+  clientId: string,
+  msgId: string,
+) => {
   const { data } = await apiUsingNow.get<{ id: string }>(
     `${clientId}/messages/${msgId}/contact`,
   );
@@ -30,10 +37,22 @@ export const retriveMessageContactWpp = async (clientId: string, msgId: string) 
   return data;
 };
 
-export const retriveMessageMediaWpp = async (clientId: string, msgId: string) => {
+export const retriveMessageMediaWpp = async (
+  clientId: string,
+  msgId: string,
+) => {
   const { data } = await apiUsingNow.get<IMediaWpp>(
     `${clientId}/messages/${msgId}/media`,
   );
 
   return data;
+};
+
+export const sendMessage = async (
+  clientId: string,
+  number: string,
+  message: string,
+  mentions: string[],
+) => {
+  await apiUsingNow.post(`${clientId}/messages`, { number, message, mentions });
 };
